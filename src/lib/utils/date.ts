d2b9011c7dde7a7be66033204af4d83339d0e119
fabioiuri <fabioiuri@live.com>
2025-03-23 22:31:02 +0000

    Fix dates to portuguese locale

--- src/lib/utils/date.ts
+++ src/lib/utils/date.ts
@@ -1,8 +1,10 @@
 import { formatDistanceToNow, parseISO, format } from "date-fns";
+import { pt } from "date-fns/locale";
 
 export const getDateDistance = (date: string) =>
   formatDistanceToNow(parseISO(date), {
     addSuffix: true,
+    locale: pt
   });
 
 export const formatDate = (
@@ -11,8 +13,8 @@ export const formatDate = (
 ) => {
   const parseDate = parseISO(date);
   if (formatDate === "short") {
-    return format(parseDate, "MMMM dd, yyyy zz");
+    return format(parseDate, "MMMM dd, yyyy zz", { locale: pt });
   }
 
-  return format(parseDate, "EEEE, MMMM d, yyyy h:mm a zz");
+  return format(parseDate, "EEEE, MMMM d, yyyy h:mm a zz", { locale: pt });
 };
